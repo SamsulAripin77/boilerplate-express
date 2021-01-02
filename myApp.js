435147471cb3e7c791506346ac62b9a12e273b63
SamsulAripin77 <arifins192@gmail.com>
2021-01-02 05:03:12 +0000

    belajar node js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,49 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
+app.use('/assets',express.static(__dirname + '/public'))
+
+app.use(function middleware(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+app.get("/", (req, res) => {
+  let absolutePath = __dirname + '/views/index.html'
+  res.sendFile(absolutePath)
+});
+
+app.get('/now',(req,res,next) => {
+  req.time = new Date().toString()
+  next()
+},(req,res) => {
+  res.send({time: req.time})
+})
+
+app.get('/json',(req,res) => {
+var message = 'Hello Json'
+if (process.env.MESSAGE_STYLE === "uppercase") {
+  message = message.toUpperCase();
+} else if (process.env.MESSAGE_STYLE === 'lowercase'){
+  message = message.toLowerCase()
+}
+res.json({"message": message})
+})
+
+app.get('/:word/echo',(req,res,next) => {
+  let word = req.params.word
+  res.send({echo: word})
+  next()
+})
+
+
+app.post('/name',(req,res,next) => {
+  let {first,last} = req.body
+  res.send({name: `${first} ${last}`})
+})
 
 
 
@@ -33,7 +76,4 @@ var app = express();
 
 
 
-
-
-
- module.exports = app;
+module.exports = app;
